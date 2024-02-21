2c7b2992ba3d20951340096cd00bf5dc6a21cca5
George Giokas <s231593@students.aie.edu.au>
2024-02-21 16:37:40 +1100

    Zorp tutorial - Part 2 Complete.

--- Zorp/Zorp/main.cpp
+++ Zorp/Zorp/main.cpp
@@ -1,46 +1,86 @@
 #include <iostream>
+#include <windows.h>
+
+const char* ESC = "\x1b";
+const char* CSI = "\x1b[";
+
+const char* TITLE = "\x1b[5;20H";
+const char* INDENT = "\x1b[5C";
+const char* YELLOW = "\x1b[93m";
+const char* MAGENTA = "\x1b[95m"; 
+const char* RESET_COLOR = "\x1b[0m";
+const char* SAVE_CURSOR_POS = "\x1b[s";
+const char* RESTORE_CURSOR_POS = "\x1b[u";
+
 
 void main()
 {
+	// Set output mode to handle virtual terminal sequences
+	DWORD dwMode = 0;
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleMode(hOut, &dwMode);
+	dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+	SetConsoleMode(hOut, dwMode);
+
 	int height = 0;
 	char firstLetterOfName = 0;
 	int avatarHP = 0;
 
-	std::cout << "Welcome to ZORP!" << std::endl; 
-	std::cout << "ZORP is a game of adventure, danger, and low cunning." << std::endl; 
-	std::cout << "It is definitely not related to any other text-based adventure game." << std::endl << std::endl;
-
+	std::cout << TITLE << MAGENTA << "Welcome to ZORP!" << RESET_COLOR << std::endl; 
+	std::cout << INDENT << "ZORP is a game of adventure, danger, and low cunning." << std::endl;
+	std::cout << INDENT << "It is definitely not related to any other text-based adventure game." << std::endl;
+	std::cout << std::endl;
 	std::cout << "First, some questions..." << std::endl;
-	std::cout << "How tall are you, in centimeters? " << std::endl;
+	std::cout << std::endl;
+	// save cursor position
+	std::cout << SAVE_CURSOR_POS;
+	std::cout << INDENT << "How tall are you, in centimeters? " << INDENT << YELLOW << std::endl;
 
 	std::cin >> height;
+	std::cout << RESET_COLOR << std::endl;
+
 	if (std::cin.fail())
 	{
-		std::cout << "You have failed the first challenge and are eaten by a grue." << std::endl;
+		std::cout << INDENT << "You have failed the first challenge and are eaten by a grue." << std::endl;
 	}
 	else
 	{
-		std::cout << "You entered " << height << std::endl;
+		std::cout << INDENT << "You entered " << height << std::endl;
 	}
-	// Clears error flags
-	std::cin.clear();
-	// Clears all chars in input buffer
-	std::cin.ignore(std::cin.rdbuf()->in_avail());
 
-	std::cout << "What is the first letter of your name ?" << std::endl;
+	// Clear input buffer
+	std::cin.clear();
+	std::cin.ignore(std::cin.rdbuf()->in_avail());
+	std::cin.get();
+
+	// Move the cursor to the start of the 1st question
+	std::cout << RESTORE_CURSOR_POS;
+	// Delete the next 4 lines of text
+	std::cout << CSI << "4m";
+
+
+	std::cout << INDENT  << "What is the first letter of your name ?" << INDENT << YELLOW;
 
 	std::cin >> firstLetterOfName;
+	std::cout << RESET_COLOR << std::endl;
+
 							// !isaplha checks if input was a-z charcter
 	if (std::cin.fail() || !isalpha(firstLetterOfName))
 	{
-		std::cout << "You have failed the second challenge and are eaten by a grue" << std::endl;
+		std::cout << INDENT <<"You have failed the second challenge and are eaten by a grue" << std::endl;
 	}
 	else
 	{
-		std::cout << "You entered " << firstLetterOfName << std::endl;
+		std::cout << INDENT << "You entered " << firstLetterOfName << std::endl;
 	}
 	std::cin.clear();
 	std::cin.ignore(std::cin.rdbuf()->in_avail());
+	std::cin.get();
+
+	// Move the cursor to the start of the 1st question
+	std::cout << RESTORE_CURSOR_POS;
+	std::cout << CSI << "A";		// cursor up 1
+	std::cout << CSI << "4M";		// delete the next 4 lines of text
 
 	if (firstLetterOfName != 0)
 	{
@@ -50,10 +90,12 @@ void main()
 	{
 		avatarHP = 0;
 	}
-	std::cout << std::endl << "Using a complex deterministic algorithm, it has been calculated that you have " << avatarHP << " hit point(s)" << std::endl;
+	std::cout << std::endl << INDENT << "Using a complex deterministic algorithm, it has been calculated that you have " << avatarHP << " hit point(s)" << std::endl;
 
-	std::cout << std::endl << "Press 'Enter' to exit the program." << std::endl;
+	std::cout << std::endl << INDENT << "Press 'Enter' to exit the program." << std::endl;
 	std::cin.get();
+
+	//system("cls"); - Used to clear the screen.
 
 	return;
 
